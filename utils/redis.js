728a8b8aecf844e973e930504d6eb1119cdaade8
PC-Ngumoha <pcngumoha@gmail.com>
2023-12-04 21:46:20 +0100

    Update: Improved the documentation for the classes and methods in the code

--- utils/redis.js
+++ utils/redis.js
@@ -6,10 +6,23 @@
 import { promisify } from 'node:util';
 import { createClient } from 'redis';
 
+/**
+ * @class RedisClient
+ * 
+ * @abstract Contains the code that handles the creation of a new redis
+ * client along with a set of helper methods needed to make use of it.
+ * 
+ * @method constructor
+ * @method isAlive
+ * @method get
+ * @method set
+ * @method del
+ */
 class RedisClient {
   constructor() {
     this.client = createClient();
     this.client.on('error', (err) => console.log(`Error: ${err}`));
+    this.client.on('connect', () => console.log('Connected Successfully'));
     this.getAsync = promisify(this.client.get).bind(this.client);
     this.setAsync = promisify(this.client.set).bind(this.client);
     this.delAsync = promisify(this.client.del).bind(this.client);
