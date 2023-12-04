7d3ef94409dc8cdcfb6d09dec0bac333995a2783
PC-Ngumoha <pcngumoha@gmail.com>
2023-12-04 16:50:12 +0100

    Update: Refactor code

--- utils/redis.js
+++ utils/redis.js
@@ -10,30 +10,25 @@ class RedisClient {
   constructor() {
     this.client = createClient();
     this.client.on('error', (err) => console.log(`Error: ${err}`));
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
-    try {
-      this.client.ping();
-      return true;
-    } catch (err) {
-      return false;
-    }
+    return this.client.connected;
   }
 
   async get(key) {
-    const getAsync = promisify(this.client.get).bind(this.client);
-    return await getAsync(key);
+    return await this.getAsync(key);
   }
 
   async set(key, value, duration) {
-    const setAsync = promisify(this.client.set).bind(this.client);
-    await setAsync(key, value, 'EX', duration);
+    await this.setAsync(key, value, 'EX', duration);
   }
 
   async del(key) {
-    const delAsync = promisify(this.client.del).bind(this.client);
-    await delAsync(key);
+    await this.delAsync(key);
   }
 }
 
